762a794b45199a303b6e5073d0ef4abc8a11fb99
YangK <1041502459@qq.com>
2020-02-05 09:42:53 +0800

    基础题库

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,78 +1,87 @@
 <template>
-  <el-row>
-    <el-col :span="6">
-      学科：
-      <el-select v-model="searchForm.subjectID">
-        <el-option
-          v-for="item in subjectIDList"
-          :key="item.value"
-          :value="item.value"
-          :label="item.label"
-        ></el-option>
-      </el-select>
-    </el-col>
-    <el-col :span="6">
-      难度：
-      <el-select v-model="searchForm.difficulty">
-        <el-option
-          v-for="item in difficultyList"
-          :key="item.value"
-          :value="item.value"
-          :label="item.label"
-        ></el-option>
-      </el-select>
-    </el-col>
-    <el-col :span="6">
-      试题类型：
-      <el-select v-model="searchForm.questionType" style="width:140px;">
-        <el-option
-          v-for="item in questionTypeList"
-          :key="item.value"
-          :value="item.value"
-          :label="item.label"
-        ></el-option>
-      </el-select>
-    </el-col>
-  </el-row>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="box-card">
+        <!-- :gutter 给各个col设置间歇，单位是像素 -->
+        <el-row :gutter="20">
+          <el-col :span="6">
+            学科:
+            <el-select v-model="searchForm.subjectID" class="wh">
+              <el-option
+                v-for="item in subjectIDList"
+                :key="item.value"
+                :value="item.value"
+                :label="item.label"
+              ></el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            难度:
+             <el-select v-model="searchForm.difficulty" class="wh">
+              <el-option
+                v-for="item in difficultyList"
+                :key="item.value"
+                :value="item.value"
+                :label="item.label"
+              ></el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            试题类型:
+            <el-select v-model="searchForm.questionType" class="wh">
+              <el-option
+                v-for="item in questionTypeList"
+                :key="item.value"
+                :value="item.value"
+                :label="item.label"
+              ></el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6"></el-col>
+        </el-row>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
-// 导入获得学科简单列表的api方法
-import { simple } from '@/api/hmmm/subjects' // 学科
-// as给导入的成员设置别名,难度试题类型
+import { simple } from '@/api/hmmm/subjects.js'
 import {
-  difficulty as difficultyList, 
-  questionType as questionTypeList} 
-  from '@/api/hmmm/constants' // 常量数据
+  difficulty as difficultyList,
+  questionType as questionTypeList
+} from '@/api/hmmm/constants' // 按需导入常量数据
 export default {
-  name: 'QuestionsList',
   data() {
     return {
-      subjectIDList: [], // 学科简单列表收集
-      difficultyList, // 简易成员赋值(difficultyList:difficultyList)
-      questionTypeList, // 简易成员赋值
-      // 搜索题库的表单数据对象
+      // 定义各个搜索表单域的数据展示成员
+      subjectIDList: [],
+      difficultyList,
+      questionTypeList,
+      // 定义搜索数据对象
       searchForm: {
-        subjectID: '', // 学科ID
+        subjectID: '',
         difficulty: '',
         questionType: ''
       }
     }
   },
   methods: {
-    // 获得 学科 下拉列表数据
-    async getSubjectIDList() {
+    // 获取学科下拉数据
+    async getsubjectIDList() {
       var rst = await simple()
-      // console.log(rst)
+      console.log(rst)
       // 把获得到的学科信息赋予给 subjectIDList 成员
       this.subjectIDList = rst.data
     }
   },
   created() {
-    this.getSubjectIDList()
+    this.getsubjectIDList()
   }
 }
 </script>
 
 <style scoped>
+.wh {
+  width: 170px;
+}
 </style>
