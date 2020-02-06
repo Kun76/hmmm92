3f2a2459703fa566497902632a90f6e6951d5b50
YangK <1041502459@qq.com>
2020-02-06 16:04:17 +0800

    添加基础图库页面设置

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">题库添加</div>
+    <div class="app-container">
+      <el-card class="box-card">
+        <el-form ref="addFormRef" :model="addForm" label-width="120px">
+          <!--:model作用：可以使得el-form针对表单的全部信息进行收集，固定属性-->
+          <el-form-item label="学科：">
+            <el-select v-model="addForm.subjectID"></el-select>
+          </el-form-item>
+          <el-form-item label="目录：">
+            <el-select v-model="addForm.catalogID"></el-select>
+          </el-form-item>
+          <el-form-item label="企业：">
+            <el-select v-model="addForm.enterpriseID"></el-select>
+          </el-form-item>
+          <el-form-item label="城市：">
+            <el-select v-model="addForm.province" @change="getCitys(addForm.province)"></el-select>
+            <el-select v-model="addForm.city"></el-select>
+          </el-form-item>
+          <el-form-item label="方向：">
+            <el-select v-model="addForm.direction"></el-select>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -8,7 +30,17 @@
 export default {
   name: 'QuestionsNew',
   data() {
-    return {}
+    return {
+      // 添加试题 表单数据对象
+      addForm: {
+        subjectID: '', // 学科
+        catalogID: '', // 二级目录
+        enterpriseID: '', // 企业
+        city: '', // 区县
+        province: '', // 城市
+        direction: '' // 方向
+      }
+    }
   }
 }
 </script>
